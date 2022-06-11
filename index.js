7d8f858fdde6bad5334d76d2fa8e4cf239264276
Siddhant Khanna <siddhantkhanna2002@gmail.com>
2022-06-11 15:07:57 +0530

    Final Commit

--- index.js
+++ index.js
@@ -19,11 +19,9 @@ app.use('/user', userRoutes)
 app.get('/', (req, res) => {
     res.send("App is Running");
 });
-const CONNECTION_URL = 'mongodb+srv://adminSiddhant:adminSiddhant123@cluster0.7bklh.mongodb.net/test?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server started on Port ${PORT}`)))
 .catch((err) => console.log(err));
 
-//mongoose.set('useFindAndModify', false);
