b0592c6eb42d991a02b2e4393fdcb8f73d93f797
Siddhant Khanna <siddhantkhanna2002@gmail.com>
2022-06-13 00:00:37 +0530

    d

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 // app.use(cors(corsOptions));
 
 
-app.use(function(req, res, next){
+app.all(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Credentials', true);
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
@@ -37,4 +38,3 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server started on Port ${PORT}`)))
 .catch((err) => console.log(err));
-
