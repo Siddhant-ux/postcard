b11c33cb5a9186e25c2d6dd87ff43e5b95121303
Siddhant Khanna <siddhantkhanna2002@gmail.com>
2022-06-12 19:16:56 +0530

    proxy url

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 // app.use(cors(corsOptions));
 
 
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "https://postcard-mern.netlify.app");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next){
+//     res.header("Access-Control-Allow-Origin", "https://postcard-mern.netlify.app");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use('/posts', postRoutes);  //adds a /posts at the end of all the routes in postRoutes
 app.use('/user', userRoutes)
