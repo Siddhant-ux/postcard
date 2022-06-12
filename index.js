4972591d31846bbc1972c224313b814ec2d5a464
Siddhant Khanna <siddhantkhanna2002@gmail.com>
2022-06-12 20:16:44 +0530

    idk

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ dotenv.config();
 
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(bodyParser.json({limit: "30mb", extended: true}));
-const corsOptions ={
-    origin:'https://postcard-mern.netlify.app', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'https://postcard-mern.netlify.app', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
 
 
-// app.use(function(req, res, next){
-//     res.header("Access-Control-Allow-Origin", "https://postcard-mern.netlify.app");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "https://postcard-mern.netlify.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use('/posts', postRoutes);  //adds a /posts at the end of all the routes in postRoutes
 app.use('/user', userRoutes)
